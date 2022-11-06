df87625697f807cae5b34657174a74f671678c21
kaznakagawa <33821811+kaznakagawa@users.noreply.github.com>
2022-11-06 23:57:38 +0900

    Add user vault map

--- x/tollroad/types/genesis.go
+++ x/tollroad/types/genesis.go
@@ -12,6 +12,7 @@ func DefaultGenesis() *GenesisState {
 	return &GenesisState{
 		SystemInfo:       SystemInfo{},
 		RoadOperatorList: []RoadOperator{},
+		UserVaultList:    []UserVault{},
 		// this line is used by starport scaffolding # genesis/types/default
 		Params: DefaultParams(),
 	}
@@ -29,6 +30,16 @@ func (gs GenesisState) Validate() error {
 			return fmt.Errorf("duplicated index for roadOperator")
 		}
 		roadOperatorIndexMap[index] = struct{}{}
+	}
+	// Check for duplicated index in userVault
+	userVaultIndexMap := make(map[string]struct{})
+
+	for _, elem := range gs.UserVaultList {
+		index := string(UserVaultKey(elem.Owner, elem.RoadOperatorIndex, elem.Token))
+		if _, ok := userVaultIndexMap[index]; ok {
+			return fmt.Errorf("duplicated index for userVault")
+		}
+		userVaultIndexMap[index] = struct{}{}
 	}
 	// this line is used by starport scaffolding # genesis/types/validate
 
